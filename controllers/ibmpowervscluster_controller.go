317ea4af51e3f2199c22afc5e3f0146fd3984ffe
Manjunath A Kumatagi <mkumatag@in.ibm.com>
2021-02-26 14:33:43 +0530

    powervs support

---
+++ controllers/ibmpowervscluster_controller.go
@@ -0,0 +1,145 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"github.com/kubernetes-sigs/cluster-api-provider-ibmcloud/cloud/scope"
+	"github.com/pkg/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	infrastructurev1alpha3 "github.com/kubernetes-sigs/cluster-api-provider-ibmcloud/api/v1alpha3"
+)
+
+// IBMPowerVSClusterReconciler reconciles a IBMPowerVSCluster object
+type IBMPowerVSClusterReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=ibmpowervsclusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=ibmpowervsclusters/status,verbs=get;update;patch
+
+func (r *IBMPowerVSClusterReconciler) Reconcile(req ctrl.Request) (_ ctrl.Result, reterr error) {
+	ctx := context.Background()
+	log := r.Log.WithValues("ibmpowervscluster", req.NamespacedName)
+
+	// your logic here
+
+	// Fetch the IBMVPCCluster instance
+	ibmCluster := &infrastructurev1alpha3.IBMPowerVSCluster{}
+	err := r.Get(ctx, req.NamespacedName, ibmCluster)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	// Fetch the Cluster.
+	cluster, err := util.GetOwnerCluster(ctx, r.Client, ibmCluster.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if cluster == nil {
+		log.Info("Cluster Controller has not yet set OwnerRef")
+		return ctrl.Result{}, nil
+	}
+
+	clusterScope, err := scope.NewPowerVSClusterScope(scope.PowerVSClusterScopeParams{
+		Client:            r.Client,
+		Logger:            log,
+		Cluster:           cluster,
+		IBMPowerVSCluster: ibmCluster,
+	})
+
+	// Always close the scope when exiting this function so we can persist any GCPMachine changes.
+	defer func() {
+		if err := clusterScope.Close(); err != nil && reterr == nil {
+			reterr = err
+		}
+	}()
+
+	// Handle deleted clusters
+	if !ibmCluster.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(clusterScope)
+	}
+
+	if err != nil {
+		return reconcile.Result{}, errors.Errorf("failed to create scope: %+v", err)
+	} else {
+		return r.reconcile(ctx, clusterScope)
+	}
+}
+
+func (r *IBMPowerVSClusterReconciler) reconcile(ctx context.Context, clusterScope *scope.PowerVSClusterScope) (ctrl.Result, error) {
+	if !controllerutil.ContainsFinalizer(clusterScope.IBMPowerVSCluster, infrastructurev1alpha3.ClusterFinalizer) {
+		controllerutil.AddFinalizer(clusterScope.IBMPowerVSCluster, infrastructurev1alpha3.ClusterFinalizer)
+		return ctrl.Result{}, nil
+	}
+
+	//if clusterScope.IBMPowerVSCluster.Status.APIEndpoint.PortID == nil {
+	//	port, err := clusterScope.CreatePort()
+	//	if err != nil {
+	//		return ctrl.Result{}, errors.Wrap(err, "failed to create a port for APIEndpoint")
+	//	}
+	//
+	//	clusterScope.IBMPowerVSCluster.Status.APIEndpoint = infrastructurev1alpha3.PowerVSAPIEndpoint{
+	//		PortID:          port.PortID,
+	//		InternalAddress: port.IPAddress,
+	//	}
+	//}
+	//
+	//if clusterScope.IBMPowerVSCluster.Status.APIEndpoint.PortID != nil && clusterScope.IBMPowerVSCluster.Status.APIEndpoint.Address == nil {
+	//	portInfo, err := clusterScope.GetPort()
+	//	if err != nil {
+	//		return ctrl.Result{}, errors.Wrap(err, "failed to GetPort")
+	//	}
+	//	if portInfo.ExternalIP != "" {
+	//		clusterScope.IBMPowerVSCluster.Status.APIEndpoint.Address = &portInfo.ExternalIP
+	//	} else {
+	//		return ctrl.Result{}, errors.Wrap(err, "failed to the external IP address for the port")
+	//	}
+	//	clusterScope.IBMPowerVSCluster.Status.Ready = true
+	//}
+	clusterScope.IBMPowerVSCluster.Status.Ready = true
+	return ctrl.Result{}, nil
+}
+
+func (r *IBMPowerVSClusterReconciler) reconcileDelete(clusterScope *scope.PowerVSClusterScope) (ctrl.Result, error) {
+	//if err := clusterScope.DeletePort(); err != nil {
+	//	return ctrl.Result{}, errors.Wrap(err, "failed to delete a port for APIEndpoint")
+	//} else {
+	controllerutil.RemoveFinalizer(clusterScope.IBMPowerVSCluster, infrastructurev1alpha3.ClusterFinalizer)
+	return ctrl.Result{}, nil
+	//}
+}
+
+func (r *IBMPowerVSClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&infrastructurev1alpha3.IBMPowerVSCluster{}).
+		Complete(r)
+}
--- go.mod
+++ go.mod
@@ -3,19 +3,26 @@ module github.com/kubernetes-sigs/cluster-api-provider-ibmcloud
 go 1.13
 
 require (
+	github.com/IBM-Cloud/bluemix-go v0.0.0-20200921095234-26d1d0148c62
+	github.com/IBM-Cloud/power-go-client v1.0.55
 	github.com/IBM/go-sdk-core v1.1.0
 	github.com/IBM/go-sdk-core/v4 v4.5.1 // indirect
 	github.com/IBM/vpc-go-sdk v0.1.1
+	github.com/davecgh/go-spew v1.1.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-logr/logr v0.1.0
-	github.com/onsi/ginkgo v1.13.0
-	github.com/onsi/gomega v1.10.1
+	github.com/onsi/ginkgo v1.15.0
+	github.com/onsi/gomega v1.10.5
 	github.com/pkg/errors v0.9.1
+	github.com/ppc64le-cloud/powervs-utils v0.0.0-20210106101518-5d3f965b0344
 	github.com/prometheus/common v0.9.1
 	google.golang.org/api v0.4.0
+	gopkg.in/square/go-jose.v2 v2.2.2
 	k8s.io/api v0.17.9
 	k8s.io/apimachinery v0.17.9
 	k8s.io/client-go v0.17.9
 	k8s.io/klog v1.0.0
+	k8s.io/klog/v2 v2.0.0
 	k8s.io/utils v0.0.0-20200619165400-6e3d28b6ed19
 	sigs.k8s.io/cluster-api v0.3.9
 	sigs.k8s.io/controller-runtime v0.5.10
